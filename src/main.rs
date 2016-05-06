8cbc9b0746bb5e998f1116e5627cfb7f5e81ffdc
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-05-06 16:28:16 +0200

    src/main.rs : write solution to file

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ extern crate n_puzzle;
 
 const DEFAULT_HEURISTIC: &'static str = "manhattan";
 
-use n_puzzle::{node, astar, parser};
-use n_puzzle::heuristic::Heuristic;
-
 use ansi_term::Colour::*;
 
 use std::error::Error;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
+
+use n_puzzle::{node, astar, parser};
+use n_puzzle::heuristic::Heuristic;
 
 fn read_files(filename: &String) -> String {
     let path = Path::new(filename);
@@ -28,6 +28,26 @@ fn read_files(filename: &String) -> String {
         Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
         Ok(_) => buffer,
     }
+}
+
+fn write_to_file(result: &String) {
+    let path = Path::new("solution.txt");
+    let display = path.display();
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                   display,
+                   Error::description(&why)),
+        Ok(file) => file,
+    };
+    // Write the `result` string to `file`, returns `io::Result<()>`
+    match file.write_all(result.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}",
+                    display,
+                    Error::description(&why)),
+        Ok(_) => println!("successfully wrote to {}", display),
+    };
+    ()
 }
 
 fn main() {
@@ -44,11 +64,15 @@ fn main() {
             .collect();
         let mut start = parser::to_node(parser::remove_comments(vec));
         if !start.is_solvable() { println!("{}", Yellow.bold().paint("This puzzle is not solvable.")); }
-        else { let goal = node::Goal::new(start.len);
+        else {
+            let goal = node::Goal::new(start.len);
+            let mut result = String::new();
             for node in astar::astar(&mut start, &goal, &heuristic).unwrap()
             {
-                println!("{}", node)
+                let node_str = format!("{}\n", node);
+                result.push_str(&node_str);
             }
+            write_to_file(&result)
         }
     }
 }
